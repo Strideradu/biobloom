51dc17ee3e85b9af35f69ef2ebd27b69295ace2d
Strideradu <dunan00001@hotmail.com>
2017-08-16 19:06:43 -0400

    some comment

---
+++ cmake-build-debug/CMakeFiles/biobloom.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/BioBloomCategorizer/BioBloomCategorizer.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/BioBloomCategorizer/BioBloomCategorizer.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/BioBloomCategorizer/BioBloomClassifier.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/BioBloomCategorizer/BioBloomClassifier.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/BioBloomCategorizer/MultiFilter.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/BioBloomCategorizer/MultiFilter.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/BioBloomCategorizer/ResultsManager.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/BioBloomCategorizer/ResultsManager.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/BioBloomMaker/BioBloomMaker.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/BioBloomMaker/BioBloomMaker.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/BioBloomMaker/BloomFilterGenerator.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/BioBloomMaker/BloomFilterGenerator.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/BioBloomMaker/WindowedFileParser.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/BioBloomMaker/WindowedFileParser.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/BloomFilter.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/BloomFilter.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/BloomFilterInfo.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/BloomFilterInfo.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/Dynamicofstream.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/Dynamicofstream.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/Fcontrol.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/Fcontrol.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/Options.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/Options.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/ReadsProcessor.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/ReadsProcessor.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/Sequence.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/Sequence.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/SignalHandler.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/SignalHandler.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/Uncompress.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/Uncompress.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/city.cc" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/city.cc.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Common/gzstream.C" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Common/gzstream.C.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/DataLayer/FastaReader.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/DataLayer/FastaReader.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/DataLayer/FastaWriter.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/DataLayer/FastaWriter.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Tests/BloomFilterCategorizerTests.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Tests/BloomFilterCategorizerTests.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Tests/BloomFilterInfoTests.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Tests/BloomFilterInfoTests.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Tests/BloomFilterMakerTests.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Tests/BloomFilterMakerTests.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Tests/BloomFilterTests.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Tests/BloomFilterTests.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Tests/ReadProcessorTests.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Tests/ReadProcessorTests.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Tests/SeqEvalTests.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Tests/SeqEvalTests.cpp.o"
+  "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/Tests/WindowedParserTests.cpp" "/cygdrive/c/Users/Nan/Documents/GitHub/biobloom/cmake-build-debug/CMakeFiles/biobloom.dir/Tests/WindowedParserTests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/biobloom.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/biobloom.dir/BioBloomCategorizer/BioBloomCategorizer.cpp.o"
+  "CMakeFiles/biobloom.dir/BioBloomCategorizer/BioBloomClassifier.cpp.o"
+  "CMakeFiles/biobloom.dir/BioBloomCategorizer/MultiFilter.cpp.o"
+  "CMakeFiles/biobloom.dir/BioBloomCategorizer/ResultsManager.cpp.o"
+  "CMakeFiles/biobloom.dir/BioBloomMaker/BioBloomMaker.cpp.o"
+  "CMakeFiles/biobloom.dir/BioBloomMaker/BloomFilterGenerator.cpp.o"
+  "CMakeFiles/biobloom.dir/BioBloomMaker/WindowedFileParser.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/BloomFilter.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/BloomFilterInfo.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/city.cc.o"
+  "CMakeFiles/biobloom.dir/Common/Dynamicofstream.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/Fcontrol.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/gzstream.C.o"
+  "CMakeFiles/biobloom.dir/Common/Options.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/ReadsProcessor.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/Sequence.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/SignalHandler.cpp.o"
+  "CMakeFiles/biobloom.dir/Common/Uncompress.cpp.o"
+  "CMakeFiles/biobloom.dir/DataLayer/FastaReader.cpp.o"
+  "CMakeFiles/biobloom.dir/DataLayer/FastaWriter.cpp.o"
+  "CMakeFiles/biobloom.dir/Tests/BloomFilterCategorizerTests.cpp.o"
+  "CMakeFiles/biobloom.dir/Tests/BloomFilterInfoTests.cpp.o"
+  "CMakeFiles/biobloom.dir/Tests/BloomFilterMakerTests.cpp.o"
+  "CMakeFiles/biobloom.dir/Tests/BloomFilterTests.cpp.o"
+  "CMakeFiles/biobloom.dir/Tests/ReadProcessorTests.cpp.o"
+  "CMakeFiles/biobloom.dir/Tests/SeqEvalTests.cpp.o"
+  "CMakeFiles/biobloom.dir/Tests/WindowedParserTests.cpp.o"
+  "biobloom.pdb"
+  "biobloom.exe"
+  "libbiobloom.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/biobloom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
